09214f0fbc458cfb1baebef37d74d63b64a2af60
Garrett Thor <garrettthor@gmail.com>
2022-09-28 11:35:33 -0700

    Create, read, update routes basically done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const Question = require('./models/question.js');
 
 mongoose.connect('mongodb://localhost:27017/menu-quiz', {
@@ -20,13 +21,58 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 app.get('/', (req,res) => {
-    res.render('home')
+    res.render('home');
 });
 
-app.get('/newquestion', (req, res) => {
-    const question = new Question
+app.get('/quiz/new', (req, res) => {
+    res.render('quiz/new');
 })
+
+app.post('/quiz', async (req, res) => {
+    const question = new Question(req.body.question);
+    await question.save();
+    res.redirect('quiz/list')
+})
+
+app.get('/quiz/adminIndex', (req, res) => {
+    res.render('quiz/adminIndex')
+})
+
+app.get('/quiz', async (req, res) => {
+    const questions = await Question.find({});
+    res.render('quiz/index', { questions });
+})
+
+app.get('/quiz/list', async (req, res) => {
+    const questions = await Question.find({});
+    res.render('quiz/list', { questions });
+})
+
+app.get('/quiz/:id', async (req, res) => {
+    const question = await Question.findById(req.params.id);
+    res.render('quiz/show', { question });
+})
+
+app.get('/quiz/:id/edit', async (req, res) => {
+    const question = await Question.findById(req.params.id)
+    res.render('quiz/edit', { question });
+})
+
+app.put('/quiz/:id', async (req, res) => {
+    const { id } = req.params;
+    const question = await Question.findByIdAndUpdate(id, { ...req.body.question });
+    res.redirect(`/quiz/${question._id}`);
+})
+
+// app.get('/newquestion', async (req, res) => {
+//     const question = new Question({ number: 1, question: 'Is this the first question?', correctAns: 'Yes', wrongAns1: 'No', wrongAns2: 'Maybe', wrongAns3: 'I dunno' })
+//     await question.save();
+//     res.send(question)
+// })
 
 app.listen(1984, () => {
     console.log('Serving on port 1984')
